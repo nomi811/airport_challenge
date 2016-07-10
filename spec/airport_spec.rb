a3e5936eaad05e6d09ec4dd80815000b286e826c
Nomi Vos <nvoswork@gmail.com>
2016-07-10 18:11:30 -0500

    sets up test to see if plane is flying and makes it pass

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,6 +5,10 @@ describe Airport do
   it 'knows when a plane is flying' do
     plane = double :plane, location: 'flying'
     expect(airport.location(plane)).to eq 'flying'
+  end
 
+  it 'knows when a plane is at the airport' do
+    plane = double :plane, location: 'airport'
+    expect(airport.location(plane)).to eq 'airport'
   end
 end
